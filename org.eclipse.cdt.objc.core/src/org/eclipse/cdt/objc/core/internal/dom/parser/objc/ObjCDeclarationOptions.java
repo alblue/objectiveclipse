9f384ccb8dfc8aea1496d968883f681dc077cacf
rrusaw <rrusaw@b0dc983c-f627-11dd-ada4-5153b8187bf2>
2009-07-26 03:33:30 +0000

    Issue 39:
    Added support for (.. in ..) {} style for loops from ObjC 2.0.
    
    git-svn-id: https://objectiveclipse.googlecode.com/svn/trunk@124 b0dc983c-f627-11dd-ada4-5153b8187bf2

--- org.eclipse.cdt.objc.core/src/org/eclipse/cdt/objc/core/internal/dom/parser/objc/ObjCDeclarationOptions.java
+++ org.eclipse.cdt.objc.core/src/org/eclipse/cdt/objc/core/internal/dom/parser/objc/ObjCDeclarationOptions.java
@@ -14,6 +14,7 @@ public class ObjCDeclarationOptions extends DeclarationOptions {
     final public static int IS_IMPLEMENTATION = 0x8000;
     final public static int IS_PROPERTY = 0x10000;
     final public static int ALLOW_PROPERTY_ATTR = 0x20000;
+    final public static int ALLOW_FAST_ENUM_EXPR = 0x40000;
 
     public static final ObjCDeclarationOptions DYNAMIC_PROPERTY_DEF = new ObjCDeclarationOptions(
             NO_INITIALIZER | NO_FUNCTIONS | NO_NESTED | IS_PROPERTY);
@@ -22,22 +23,24 @@ public class ObjCDeclarationOptions extends DeclarationOptions {
             NO_FUNCTIONS | NO_NESTED | IS_PROPERTY);
 
     public static final ObjCDeclarationOptions IMPLEMENTATION_LIST = new ObjCDeclarationOptions(
-            ALLOW_PROPERTY | NO_INITIALIZER | IS_IMPLEMENTATION | ALLOW_METHOD);
+            ALLOW_PROPERTY | NO_INITIALIZER | IS_IMPLEMENTATION | ALLOW_METHOD | ALLOW_PROTOCOL_MODIFIERS);
 
     public static final ObjCDeclarationOptions METHOD_PARAMETER = new ObjCDeclarationOptions(
-            ALLOW_ABSTRACT | METHOD_PARAM);
-
+            ALLOW_ABSTRACT | METHOD_PARAM | ALLOW_PROTOCOL_MODIFIERS);
+    
     public static final ObjCDeclarationOptions INSTANCE_VARIABLES = new ObjCDeclarationOptions(
             ALLOW_ABSTRACT | NO_INITIALIZER | ALLOW_VISIBILITY);
 
     public static final ObjCDeclarationOptions INTERFACE_LIST = new ObjCDeclarationOptions(
-            ALLOW_PROPERTY | NO_INITIALIZER | ALLOW_METHOD);
+            ALLOW_PROPERTY | NO_INITIALIZER | ALLOW_METHOD | ALLOW_PROTOCOL_MODIFIERS);
 
     public static final ObjCDeclarationOptions PROPERTY_DECL = new ObjCDeclarationOptions(
             NO_FUNCTIONS | NO_NESTED | IS_PROPERTY | ALLOW_PROPERTY_ATTR);
 
     public static final ObjCDeclarationOptions PROTOCOL_LIST = new ObjCDeclarationOptions(
-            REQUIRE_ABSTRACT | NO_INITIALIZER | ALLOW_OPTIONALITY | ALLOW_METHOD);
+            REQUIRE_ABSTRACT | NO_INITIALIZER | ALLOW_OPTIONALITY | ALLOW_METHOD | ALLOW_PROTOCOL_MODIFIERS);
+    
+    public static final ObjCDeclarationOptions FAST_ENUMERATOR = new ObjCDeclarationOptions(ALLOW_FAST_ENUM_EXPR | ALLOW_CONSTRUCTOR_INITIALIZER);
     
     final public boolean fIsMethodParameter;
     final public boolean fIsProperty;
@@ -48,6 +51,7 @@ public class ObjCDeclarationOptions extends DeclarationOptions {
     final public boolean fAllowProtocolParamModifiers;
     final public boolean fAllowPropertyAttributes;
     final public boolean fIsImplementation;
+    final public boolean fAllowFastEnumeratorExpression;
 
     public ObjCDeclarationOptions(int options) {
         super(options);
@@ -60,6 +64,7 @@ public class ObjCDeclarationOptions extends DeclarationOptions {
         fAllowProtocolParamModifiers = ((options & ALLOW_PROTOCOL_MODIFIERS) != 0) ? true : false;
         fIsProperty = ((options & IS_PROPERTY) != 0) ? true : false;
         fAllowPropertyAttributes = ((options & ALLOW_PROPERTY_ATTR) != 0) ? true : false;
+        fAllowFastEnumeratorExpression = ((options & ALLOW_FAST_ENUM_EXPR) != 0) ? true : false;
     }
 
 }
